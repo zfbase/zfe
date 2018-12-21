c8904ef35be21f9569143c94acb7394bfb1c800e
Ilya Serdyuk <ilya@serdyuk.pro>
2018-12-21 13:09:04 +0300

    Отключил значки валидации из элементов форм – достаточно цветовой идентификации

---
+++ library/ZFE/Form/Decorator/Feedback/State.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * ZFE – платформа для построения редакторских интерфейсов.
+ */
+
+/**
+ * Отключил отображение иконки статуса валидации
+ */
+class ZFE_Form_Decorator_Feedback_State extends Twitter_Bootstrap3_Form_Decorator_Feedback
+{
+}
