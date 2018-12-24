4a47bb4adf7d2ee430040f14e74da83e2bbbef97
Ilya Serdyuk <ilya@serdyuk.pro>
2018-12-24 14:36:14 +0300

    Заменил помощник вюшек acl на IsAllowedMe

---
+++ library/ZFE/View/Helper/IsAllowedMe.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * ZFE – платформа для построения редакторских интерфейсов.
+ */
+
+/**
+ * Помощник проверки доступа.
+ */
+class ZFE_View_Helper_IsAllowedMe extends Zend_View_Helper_Abstract
+{
+    /**
+     * Проверить наличие доступа.
+     *
+     * @return bool
+     */
+    public function isAllowedMe($resource = null, $privilege = null)
+    {
+        return Zend_Registry::get('acl')->isAllowedMe($resource, $privilege);
+    }
+}
