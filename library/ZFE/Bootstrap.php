ddd8e1b301dee610b7b5183fb9e691d7d3cdb4bb
Ilya Serdyuk <ilya@serdyuk.pro>
2019-08-22 16:33:22 +0300

    Fix

--- library/ZFE/Bootstrap.php
+++ library/ZFE/Bootstrap.php
@@ -62,7 +62,9 @@ class ZFE_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $config = new Zend_Config($this->getOptions(), true);
 
         foreach ($this->_configFiles as $file) {
-            $config->merge(new Zend_Config_Ini($file, APPLICATION_ENV));
+            if (is_readable($file)) {
+                $config->merge(new Zend_Config_Ini($file, APPLICATION_ENV));
+            }
         }
 
         return $config;
