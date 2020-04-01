7323628f1333601d2e8e225dd3f792b4d29ee359
Ilya Serdyuk <ilya@serdyuk.pro>
2020-04-01 11:14:24 +0300

    Добавил в ZFE_Debug::dump() отображение места вызова в консольный вывод

--- library/ZFE/Debug.php
+++ library/ZFE/Debug.php
@@ -21,13 +21,15 @@ class ZFE_Debug extends Zend_Debug
     public static function dump($var, $label = null, $echo = true)
     {
         $output = parent::dump($var, $label, false);
+        list($place) = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
 
         if ('cli' !== self::getSapi() && !extension_loaded('xdebug')) {
-            list($place) = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
             $preOpen = '<pre class="zfe-dump" data-place="' . $place['file'] . ':' . $place['line'] . '">';
 
             $output = preg_replace('/^<pre>/', $preOpen . '<code>', $output);
             $output = preg_replace('/<\/pre>$/', '</code></pre>', $output);
+        } else {
+            $output = $place['file'] . ':' . $place['line'] . $output;
         }
 
         if ($echo) {
