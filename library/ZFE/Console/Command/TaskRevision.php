a4dba4da80f1acc3058b1b1a1400b3d5aa4a7395
Ilya Serdyuk <ilya@serdyuk.pro>
2020-05-18 11:01:02 +0300

    Консольная команда для перезапланирования отложенной задачи

---
+++ library/ZFE/Console/Command/TaskRevision.php
@@ -0,0 +1,27 @@
+<?php
+
+class ZFE_Console_Command_TaskRevision extends ZFE_Console_Command_Abstract
+{
+    protected static $_description = 'Перезапланировать отложенную задачу';
+    protected static $_help = 'Команда ожидает один параметр – ID задачи для перезапуска';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(array $params = [])
+    {
+        if (count($params) !== 1) {
+            echo '<error>' . static::$_help . "</error>\n";
+            return;
+        }
+
+        $id = $params[0];
+        $task = Tasks::hardFind($id);
+        if (!$task) {
+            echo "<error>Задача с ID #{$id} не найдена</error>\n";
+        }
+
+        $reTask = ZFE_Tasks_Manager::getInstance()->revision($task);
+        echo "<info>Добавлена новая задача с ID #{$reTask->id}</info>\n";
+    }
+}
