95aeccb094fb7d28579ad6b9bbf3faaaa2417895
Ilya Serdyuk <ilya@serdyuk.pro>
2020-06-23 11:18:30 +0300

    Добавил универсальный форматировщик даты и времени ZFE::dateTime()

--- library/ZFE/View/Helper/DateTime.php
+++ library/ZFE/View/Helper/DateTime.php
@@ -10,13 +10,6 @@
 class ZFE_View_Helper_DateTime extends Zend_View_Helper_Abstract
 {
     /**
-     * Форматы.
-     *
-     * @var array[string]
-     */
-    protected static $_format;
-
-    /**
      * Форматировать дату (и время).
      *
      * @param string $dateTime
@@ -26,20 +19,6 @@ class ZFE_View_Helper_DateTime extends Zend_View_Helper_Abstract
      */
     public function dateTime($dateTime, $time = true)
     {
-        if (in_array($dateTime, ['0000-00-00', '0000-00-00 00:00:00'])) {
-            return '';
-        }
-
-        $timestamp = strtotime($dateTime);
-        if (!$timestamp) {
-            return '';
-        }
-
-        if (null === self::$_format) {
-            self::$_format['datetime'] = Zend_Registry::get('config')->format->datetime;
-            self::$_format['date'] = Zend_Registry::get('config')->format->date;
-        }
-
-        return date(self::$_format[$time ? 'datetime' : 'date'], $timestamp);
+        return ZFE_Utilities::formatDateTime($dateTime, $time);
     }
 }
