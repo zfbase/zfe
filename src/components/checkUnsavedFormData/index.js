1bb3b821abd017bde60dfbbda8aabd426f19aa01
Ilya Serdyuk <ilya@serdyuk.pro>
2020-07-06 16:29:42 +0300

    Доработал контроль перехода со страницы без сохранения формы – теперь это плагин jQuery с управляемой логикой

--- src/components/checkUnsavedFormData/index.js
+++ src/components/checkUnsavedFormData/index.js
@@ -1,23 +1,94 @@
 import $ from 'jquery';
 
-export default (container) => {
-  $(container).find('.form-edit').each((i, el) => {
-    const form = $(el);
-    let snapshot = form.serialize();
-    form.on('submit', () => {
-      snapshot = form.serialize();
-    });
-    form.find('a').on('click', (e) => {
-      const href = e.currentTarget.getAttribute('href');
-      if (href.indexOf('/delete/' !== -1)) {
-        snapshot = form.serialize();
-      }
-    });
+const comparer = (otherArray) => {
+  return (current) => {
+    return otherArray.filter((other) => {
+      return other.name === current.name && other.value === current.value;
+    }).length === 0;
+  };
+};
+
+class CheckUnsavedFormData {
+  constructor(form) {
+    this.$form = $(form);
+    this.freeSnapshot = [];
+
+    this.setFree();
+    this.initHandlers();
+  }
+
+  initHandlers() {
+    const component = this;
+    component.$form.on('submit', () => component.setFree());
+
     window.addEventListener('beforeunload', (e) => {
-      if (form.serialize() !== snapshot) {
+      if (!component.isFree()) {
         e.preventDefault();
         e.returnValue = true;
       }
     });
+  }
+
+  isFree() {
+    const snapshot = this.$form.serializeArray();
+    return (this.freeSnapshot.filter(comparer(snapshot)).length === 0)
+      && (snapshot.filter(comparer(this.freeSnapshot)).length === 0);
+  }
+
+  setFree() {
+    this.freeSnapshot = this.$form.serializeArray();
+  }
+
+  setFreeValue(key, value) {
+    let index = null;
+    this.freeSnapshot.forEach((field, i) => {
+      if (field.name === key) {
+        index = i;
+      }
+    });
+
+    if (value === null) {
+      if (index !== null) {
+        this.freeSnapshot = this.freeSnapshot.filter((_, i) => index !== i);
+      }
+      return;
+    }
+
+    const newValue = (typeof value === 'undefined')
+      ? this.$form.serializeArray().reduce((result, field) => (field.name === key ? field.value : result), null)
+      : value;
+
+    if (index !== null) {
+      this.freeSnapshot[index].value = newValue;
+    } else {
+      this.freeSnapshot.push({ name: key, value: newValue });
+    }
+  }
+
+  getFreeSnapshot() {
+    return this.freeSnapshot;
+  }
+}
+
+$.fn.checkUnsavedFormData = function checkUnsavedFormData(command = '', ...args) {
+  const results = [];
+  const $elements = this.each((i, el) => {
+    const $this = $(this);
+
+    let $element = $this.data('plugin_checkUnsavedFormData');
+    if (!$element) {
+      if (el.tagName === 'FORM') {
+        $element = new CheckUnsavedFormData($this);
+        $this.data('plugin_checkUnsavedFormData', $element);
+      } else {
+        window.console.warn(el, '- is incorrect tag for $.fn.checkUnsavedFormData');
+      }
+    }
+
+    if (command) {
+      results.push($element[command](...args));
+    }
   });
+
+  return results.length ? results : $elements;
 };
--- src/js/zfe.js
+++ src/js/zfe.js
@@ -9,7 +9,7 @@ import '../lib/jquery.tmpl';
 import '../components/audio';
 import '../components/autocomplete/autocomplete';
 import '../components/autocomplete/multiautocomplete';
-import initCheckUnsavedFormData from '../components/checkUnsavedFormData';
+import '../components/checkUnsavedFormData';
 import '../components/consoleManager';
 import debug from '../components/debug';
 import '../components/duplicates';
@@ -126,7 +126,9 @@ const ZFE = {
   },
 
   /** Запретить переход со страницы при не сохраненных изменениях */
-  initCheckUnsavedFormData,
+  initCheckUnsavedFormData: (container) => {
+    $('.form-edit', container).checkUnsavedFormData();
+  },
 
   /** Настроить автоматическую высоту многострочных текстовых полей */
   initConfirm: (container) => {
