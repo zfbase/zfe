c29444d1881b5e81a8cd9e03b215bbfd49d4203c
Ilya Serdyuk <ilya@serdyuk.pro>
2020-08-26 17:21:12 +0300

    Добавил заглушки для модели истории

---
+++ assets/mocks/Application/Model/History.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * ZFE – платформа для построения редакторских интерфейсов.
+ */
+
+/**
+ * История.
+ */
+class History extends ZFE_Model_Default_History
+{
+}
---
+++ assets/mocks/Application/Model/generated/BaseHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * ZFE – платформа для построения редакторских интерфейсов.
+ */
+
+/**
+ * Базовый класс истории, автоматически генерируемый Doctrine ORM Framework.
+ */
+class BaseHistory extends AbstractRecord
+{
+}
