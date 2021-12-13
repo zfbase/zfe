4c37917fb4cf06e020a6444875219c5a65a99f9f
Ilya Serdyuk <ilya@serdyuk.pro>
2021-12-13 23:01:56 +0300

    Добавил очистку условий сортировки для подсчета числа результатов поиска в Sphinx

--- library/ZFE/Sphinx/Pager.php
+++ library/ZFE/Sphinx/Pager.php
@@ -8,13 +8,16 @@ class ZFE_Sphinx_Pager extends Doctrine_Pager
 {
     protected function _initialize($params = [])
     {
-        // retrieve the number of items found
+        /** @var Foolz\SphinxQL\SphinxQL $countQuery */
         $countQuery = clone $this->getCountQuery();
-        $count = $countQuery->setSelect('COUNT(*)')->execute()->fetchNum()[0];
-        //$count = $countQuery->count($this->getCountQueryParams($params));
+        $count = $countQuery
+            ->setSelect('COUNT(*)')
+            ->resetOrderBy()
+            ->execute()
+            ->fetchNum()[0];
 
         $this->_setNumResults($count);
-        $this->_setExecuted(true); // _adjustOffset relies of _executed equals true = getNumResults()
+        $this->_setExecuted(true);
 
         $this->_adjustOffset();
     }
