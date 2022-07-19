4c60a0f6bce971710419cec9b1ac55b3e09e42d1
Ilya Serdyuk <ilya@serdyuk.pro>
2022-07-19 19:25:57 +0300

    Исправил интерфейс обработчика завершения исполнения партии отложенных задач

--- library/ZFE/Tasks/Handler/PostIterationInterface.php
+++ library/ZFE/Tasks/Handler/PostIterationInterface.php
@@ -5,5 +5,5 @@ interface ZFE_Tasks_Handler_PostIterationInterface
     /**
      * @param array $performers перечень обработчиков для которых пройден цикл обработки
      */
-    public static function iteration(array $performers = []);
+    public static function execute(array $performers = []);
 }
