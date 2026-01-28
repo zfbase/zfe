8d09175b5ab5d54729c880dc1b99cd34f52768e6
Dmitry Dyomin <dma01x@gmail.com>
2026-01-28 12:11:23 +0300

    fix: починить addSelectElement в php 8.5

--- library/ZFE/Form/Helpers/Templates.php
+++ library/ZFE/Form/Helpers/Templates.php
@@ -518,7 +518,7 @@ trait ZFE_Form_Helpers_Templates
         }
 
         if (key_exists('emptyValueLabel', $options) && $options['emptyValueLabel'] !== false) {
-            $emptyValue = $options['emptyValue'] ?? null;
+            $emptyValue = $options['emptyValue'] ?? '';
             $emptyLabel = $options['emptyValueLabel'];
             $options['multiOptions'] = [$emptyValue => $emptyLabel] + $columnOptions['multiOptions'];
             unset($options['emptyValueLabel'], $options['emptyValue']);
